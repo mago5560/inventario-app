81230d98468b0a184f4b27abfae9c74f51fe1d3f
morozco <marcos.orozco@gmail.com>
2022-02-18 09:44:04 -0600

    boton install app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StorageService } from './service/storage.service';
+import { SwUpdate } from '@angular/service-worker';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Inventario';
+export class AppComponent implements OnInit {
 
-  constructor(public storageService: StorageService){}
+  title = 'Inventario'
+
+  
+  constructor(
+        public storageService: StorageService,
+        private swUpdate: SwUpdate        
+        ){}
+
+  ngOnInit() {
+      this.updatePWA();
+  }
+        
+  updatePWA(){
+    this.swUpdate.available.subscribe(value =>{
+      console.log('update:', value);
+      window.location.reload();
+    })
+
+  }
 
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 import { ROUTES } from '../sidebar/sidebar.component';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { Router } from '@angular/router';
@@ -13,13 +13,41 @@ export class NavbarComponent implements OnInit {
   public focus;
   public listTitles: any[];
   public location: Location;
-  constructor(location: Location,  private element: ElementRef, private router: Router,public storageService: StorageService) {
+  public installEvent:any = null
+
+
+  constructor(location: Location,  
+    private element: ElementRef, 
+    private router: Router,
+    public storageService: StorageService) {
     this.location = location;
   }
 
+  
+
   ngOnInit() {
     this.listTitles = ROUTES.filter(listTitle => listTitle);
+    console.log("variable null " + this.installEvent)
   }
+
+  @HostListener('window:beforeinstallprompt',['$event'])
+  onBeforeInstallPrompt(event:Event){
+    console.log('before install prompt')
+    console.log(event)
+    event.preventDefault()
+    this.installEvent = event
+  }
+
+  installByUser(){
+    if(this.installEvent) {
+      this.installEvent.prompt()
+      this.installEvent.userChoice
+        .then(rta => {
+          console.log(rta)
+        })
+    }
+  }
+
   getTitle(){
     var titlee = this.location.prepareExternalUrl(this.location.path());
     if(titlee.charAt(0) === '#'){
@@ -33,5 +61,10 @@ export class NavbarComponent implements OnInit {
     }
     return '';
   }
+
+ 
+
+
+
 
 }
